c461775c2cc85bc643901fd73950416c866534d5
ram saloke <ramsaloke117@gmail.com>
2024-06-02 17:03:55 +0530

    updated image values in Schema.js

--- Schema.js
+++ Schema.js
@@ -1,9 +1,12 @@
 const Joi = require('joi');
-module.exports.listingShema = Joi.object({
+module.exports.listingSchema = Joi.object({
     listing : Joi.object({
     title : Joi.string().required(),
     description : Joi.string().required(),
-    image : Joi.string().allow("", null),
+    image: Joi.object({
+        filename: Joi.string().allow("", null),
+        url: Joi.string().uri().allow("", null)
+    }).allow(null),
     price : Joi.number().required().min(0), 
     country : Joi.string().required(),
     location : Joi.string().required(),
