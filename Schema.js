5db95cab5b46eadc1fff15d0156d94af0050a848
ram saloke <ramsaloke117@gmail.com>
2024-06-02 09:09:21 +0530

    added a Schema.js for form validation

---
+++ Schema.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+module.exports.listingShema = Joi.object({
+    listing : Joi.object({
+    title : Joi.string().required(),
+    description : Joi.string().required(),
+    image : Joi.string().allow("", null),
+    price : Joi.number().required().min(0), 
+    country : Joi.string().required(),
+    location : Joi.string().required(),
+    }).required() 
+});
+
+module.exports
